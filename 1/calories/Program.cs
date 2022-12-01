1f12b9599606635ecdfe7b8ac6f521c6e20e8250
Sandra Scheipl <sscheipl.mmt-b2022@fh-salzburg.ac.at>
2022-12-01 19:14:04 +0100

    Part 2

---
+++ 1/calories/Program.cs
@@ -0,0 +1,34 @@
+﻿
+
+string[] lines = File.ReadAllLines("input.txt");  
+
+int max = 0;
+int sum = 0;
+int buff;
+int len = lines.Length;
+int [] maxValues = new int [len];
+int i = 0; 
+
+foreach (string line in lines) 
+{
+    if(line == "") {
+        if(max < sum) {
+            max = sum;
+            maxValues[i++] = max;
+        }
+        sum = 0;
+    }
+    int.TryParse(line, out buff);
+    sum += buff;
+} 
+
+Array.Resize(ref maxValues, i);
+foreach (int item in maxValues)
+{
+    Console.WriteLine(item);
+}
+int topThree = maxValues[i-1] + maxValues[i - 2] + maxValues[i -3] + maxValues[i -4];
+Console.WriteLine("Maximum: " + max);
+Console.WriteLine("MaxThree: " + topThree);
+
+

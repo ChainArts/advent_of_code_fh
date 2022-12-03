73a94866512577154a5344091073edd385e4d02b
Maximilian Roll <mroll.mmt-b2022@fh-salzburg.ac.at>
2022-12-03 18:13:56 +0100

    added 2,3

---
+++ 2/RPS/Program.cs
@@ -0,0 +1,41 @@
+﻿const int win = 6;      //win
+const int draw = 3;    //draw
+const int lost = 0;     //loose
+
+const int rock = 1;     //eA pX 1
+const int paper = 2;    //eB pY 2
+const int scissors = 3; //eC pZ 3
+
+int points = 0;
+
+bool check = true;
+
+foreach (string line in System.IO.File.ReadLines(@"input.txt"))
+{
+
+    if(line == "A X"){ // loose mit scissors
+        points += lost + scissors;
+    }else if(line == "A Y"){ // draw mit rock
+        points += draw + rock;
+    }else if(line == "A Z"){ // win mit papars
+        points += win + paper;
+    }else if(line == "B X"){ // loose mit rock
+        points += lost + rock;
+    }else if(line == "B Y"){ // draw mit paper
+        points += draw + paper;
+    }else if(line == "B Z"){ // win mit scissors
+        points += win + scissors;
+    }else if(line == "C X"){ // loose mit paper
+        points += lost + paper;
+    }else if(line == "C Y"){ // draw mit scissors
+        points += draw + scissors;
+    }else if(line == "C Z"){ // win mit rock
+        points += win + rock;
+    }else{
+        check = false;
+    }
+
+}
+
+System.Console.WriteLine(check);
+System.Console.WriteLine(points); 
---
+++ 3/rucksack/Program.cs
@@ -0,0 +1,50 @@
+﻿string[] text = File.ReadAllLines("./input.txt");
+
+/******** PART ONE ********/
+
+int PartOne()
+{
+    int value = 0;
+    string[] half = new string[2];
+    foreach (var line in text)
+    {
+        half[0] = line[..(line.Length / 2)];
+        half[1] = line[(line.Length / 2)..];
+
+        for (int j = 0; j < half[0].Length; j++)
+        {
+            for (int k = 0; k < half[1].Length; k++)
+            {
+                if (half[0][j] == half[1][k])
+                {
+                    value += char.IsUpper(half[0][j]) ? half[0][j] - 38 : half[0][j] - 96;
+                    k = half[1].Length;
+                    j = half[0].Length;
+                }
+            }
+        }
+    }
+
+    return value;
+}
+
+/******** PART TWO ********/
+
+int PartTwo()
+{
+    int value = 0;
+    for (int i = 0; i < text.Length; i += 3)
+    {
+        for (int k = 0; k < text[i].Length; k++) // k = current char in line
+        {
+            if (!text[i + 1].Contains(text[i][k]) || !text[i + 2].Contains(text[i][k])) continue;
+            value += char.IsUpper(text[i][k]) ? text[i][k] - 38 : text[i][k] - 96;
+            break;
+        }
+    }
+
+    return value;
+}
+
+Console.WriteLine(PartOne());
+Console.WriteLine(PartTwo());
